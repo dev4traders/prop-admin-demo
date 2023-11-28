8027482a0551b2c0e09624d15799cc7f65077308
mikha-dev <mikha.dev@gmail.com>
2023-11-28 20:14:29 +0300

    row action

--- app/Admin/Actions/Grid/ResetPasswordRowAction.php
+++ app/Admin/Actions/Grid/ResetPasswordRowAction.php
@@ -2,17 +2,16 @@
 
 namespace App\Admin\Actions\Grid;
 
-use App\Admin\Forms\ResetPassword as ResetPasswordForm;
 use Dcat\Admin\Widgets\Modal;
 use Dcat\Admin\Grid\RowAction;
+use App\Admin\Forms\ResetPasswordForm;
 
-class ResetPassword extends RowAction
+class ResetPasswordRowAction extends RowAction
 {
     protected $title = '修改密码';
 
     public function render()
     {
-        // 实例化表单类并传递自定义参数
         $form = ResetPasswordForm::make()->payload(['id' => $this->getKey()]);
 
         return Modal::make()
