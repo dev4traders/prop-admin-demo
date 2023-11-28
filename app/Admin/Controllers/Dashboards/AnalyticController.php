dee5af536dd03b254e46b5587e1a594b51015f23
mikha-dev <mikha.dev@gmail.com>
2023-11-28 11:18:32 +0300

    added profit cards

--- app/Admin/Controllers/Dashboards/AnalyticController.php
+++ app/Admin/Controllers/Dashboards/AnalyticController.php
@@ -2,41 +2,34 @@
 
 namespace App\Admin\Controllers\Dashboards;
 
+use Faker\Factory;
+use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Layout\Row;
-use Dcat\Admin\Layout\Column;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Enums\RouteAuth;
 use Dcat\Admin\Widgets\ImageAdv;
 use App\Http\Controllers\Controller;
+use Dcat\Admin\Widgets\IconWithToolTip;
+use Dcat\Admin\Widgets\Cards\ProfitCard;
 use Dcat\Admin\Widgets\Cards\PictureCard;
 
 class AnalyticController extends Controller
 {
     public function index(Content $content)
     {
+        $faker = Factory::create();
         return $content
             ->header('Analytics Dashboard')
             ->description('Analytics Dashboard...')
-            ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(new PictureCard(
-                        __('admin.title'),
-                        __('admin.description'),
-                        admin_route(RouteAuth::SETTINGS()),
-                        new ImageAdv('/vendor/dcat-admin/img/illustrations/man-with-laptop-light.png', '/vendor/dcat-admin/img/illustrations/man-with-laptop-dark.png', 'man-with-laptop-light'))
-                    );
-                    //$column->row(new Examples\Tickets());
-                });
-
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                      //  $row->column(6, new Examples\NewUsers());
-//                        $row->column(6, new Examples\NewDevices());
-                    });
-
-  //                  $column->row(new Examples\Sessions());
-  //                  $column->row(new Examples\ProductOrders());
-                });
+            ->body(function (Row $row) use($faker) {
+                $row->column(8, new PictureCard(
+                    __('admin.title'),
+                    __('admin.description'),
+                    admin_route(RouteAuth::SETTINGS()),
+                    new ImageAdv('/vendor/dcat-admin/img/illustrations/man-with-laptop-light.png', '/vendor/dcat-admin/img/illustrations/man-with-laptop-dark.png', 'man-with-laptop-light'))
+                );
+                $row->column(2, (new ProfitCard('Profit', '$2000', '78%', DcatIcon::HOME, new IconWithToolTip(DcatIcon::HELP, $faker->text(100)), admin_route(RouteAuth::SETTINGS())))->growing());
+                $row->column(2, (new ProfitCard('Sales', '-$2000', '-78%', DcatIcon::HOME, new IconWithToolTip(DcatIcon::HELP, $faker->text(100)), admin_route(RouteAuth::SETTINGS())))->growing(false));
             });
     }
 }
